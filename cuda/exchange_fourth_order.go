b8125e601e319bd1833f500d90e29dabb58b62b4
sandracshaju <122293869+sandracshaju@users.noreply.github.com>
2023-02-03 15:37:20 +0100

    Update exchange_fourth_order.go

--- cuda/exchange_fourth_order.go
+++ cuda/exchange_fourth_order.go
@@ -1,1 +1,21 @@
+package cuda
 
+import (
+	"unsafe"
+
+	"github.com/mumax/3/data"
+)
+
+// Add NNN exchange field to Beff.
+// see exchange_fourth_order.cu
+func AddExchangeFourthOrder(B, m *data.Slice, A__ SymmLUT, Br__ SymmLUT, C__ SymmLUT, Msat MSlice, regions *Bytes, mesh *data.Mesh) {
+	c := mesh.CellSize()
+	N := mesh.Size()
+	pbc := mesh.PBC_code()
+	cfg := make3DConf(N)
+	k_addexchangefourthorder_async(B.DevPtr(X), B.DevPtr(Y), B.DevPtr(Z),
+		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
+		Msat.DevPtr(0), Msat.Mul(0),
+    unsafe.Pointer(A__), unsafe.Pointer(Br__), unsafe.Pointer(C__), regions.Ptr,
+		float32(c[X]), float32(c[Y]), float32(c[Z]), N[X], N[Y], N[Z], pbc, cfg)
+}
